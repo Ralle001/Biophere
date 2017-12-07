7788cbcecfd24c3e3fda52a69a64dbef3a29e8f6
Donat Treszler <donat12@t-online.hu>
2017-12-07 20:42:02 +0100

    ....

---
+++ Biophere/SettingsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsViewController.swift
+//  Biophere
+//
+//  Created by Coder on 2017. 11. 06..
+//  Copyright © 2017. Pliz Help. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var intxt: UITextField!
+    var num: Int? = nil
+    var minusstr: String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let ViewController = segue.destination as? ViewController
+        // Ez egy rossz paradigma, erdemesebb egy kulon getterbe tenni, ha muszaj akkor inkabb igy:
+        ViewController?.waterVolume = Int(intxt.text!) ?? nil
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
--- Biophere/ViewController.swift
+++ Biophere/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textview: UITextField!
+    @IBOutlet weak var conv: UILabel!
+    
+    // Teljsen felesleges duplán tárolni ezeket a szövegeket, az UI komponensek már tárolják
+    var waterVolume: Int? = nil // Csupán az aktuális átváltott értéket érdemes tárolni, de talán még ezt sem
+    // mivel nagyon olcsó újraszámitani
+    
+    
+    
+    @IBAction func pass(_ sender: Any) {
+        // Ez a függvény csupán egy új lokális változót deklarál, semmi köze sincs az osztály tagjaihoz,
+        // ezért simán kivehetjük őket
+        //        var txt: String {
+        //            get {
+        //                return textview.text ?? ""
+        //            }
+        //            set {
+        //                textview.text = newValue
+        //            }
+        //        }
+        //conv.text = txt
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Mivel mindkét érték nil, ezért nincs értelme ennek a két hivásnak
+        //label.text = text
+        //txt = textview.text!
+        
+        
     }
-
+    
+    @IBAction func saveButton(_ sender: Any) {
+        // Ha az értéket csak a gomb lenyomása után szeretnénk kiszámolni akkor felesleges a fentebbi pass függvény
+        if let volumeText = self.textview.text, let convertedValue = Int(volumeText) {
+            self.waterVolume = convertedValue
+            self.conv.text = volumeText
+        }
+        //        if let textint = Int(text!), let txtint = Int(txt!) {
+        //            convert = String(textint - txtint)
+        //            conv.text = convert
+        //        } else{
+        //            //handle it
+        //        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
